248480395aaa3613422323e7cf820ceb35ec1e9e
Hyf <icebggg@qq.com>
2022-02-12 00:33:17 -0800

    something

--- temp.c
+++ temp.c
@@ -2,8 +2,10 @@
 #include<stdlib.h>
 int main()
 {
-SIG_IGN:
-SIG_IGN:
-
+	SIG_IGN;
+	SIG_IGN;
+	sem_t;
+	pthread_mutex_t;
+	NULL;
 	exit(0);
 }
---
+++ temp.cpp
@@ -0,0 +1,19 @@
+/*
+class myclass
+{
+public:
+myclass(){std::cout <<  "myclass()" <<std::endl;}
+~myclass(){std::cout <<  "~myclass()" <<std::endl;}
+};
+int k;
+*/
+#include<iostream>
+#include<cstdio>
+int main()
+{
+	int a[10] = {0,1,2,3,4,5,6,7,8,9};
+	int i;
+		//std::cout<<&i<<"\n";
+		//std::cout<<a+10<<"\n";
+	return 0;
+}
