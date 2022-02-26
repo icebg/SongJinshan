510d7cfedf4987807595ae6ff67a29bdc980cbd5
Hyf <icebggg@qq.com>
2022-02-26 02:12:14 -0800

    ...

--- temp.cpp
+++ temp.cpp
@@ -7,12 +7,17 @@
    };
    int k;
    */
-#include<iostream>
-#include<cstdio>
+//#include<iostream>
+//#include<cstdio>
 int main()
 {
+	int k = 10;
 	int a[10] = {0,1,2,3,4,5,6,7,8,9};
-	int i;
+	for(int i=0;i<10;++i)
+	{
+		 
+	}
+	int * p = &i;
 	//std::cout<<&i<<"\n";
 	//std::cout<<a+10<<"\n";
 	return 0;
